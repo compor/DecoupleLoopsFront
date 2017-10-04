764ffa24d6871846a129696f0e4a5ed98949efd1
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-10-04 14:55:03 +0100

    add payload weights for instructions

--- include/DecoupleLoopsFrontWeights.hpp
+++ include/DecoupleLoopsFrontWeights.hpp
@@ -5,4 +5,22 @@
 #ifndef DECOUPLELOOPSFRONTWEIGHTS_HPP
 #define DECOUPLELOOPSFRONTWEIGHTS_HPP
 
+namespace icsa {
+
+namespace IteratorRecognition {
+
+enum class PayloadWeights : unsigned {
+  Cast = 1,
+  DebugIntrinic = 1,
+  Instruction = 2,
+  Call = 10,
+  IndirectCall = 15,
+  Memory = 20,
+  MemoryIntrinsics = 20
+};
+
+} // namespace IteratorRecognition
+
+} // namespace icsa
+
 #endif // header
